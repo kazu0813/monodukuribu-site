f092cb75fa64b715bd1e98d369564736e718e458
kazu0813 <kazu.gamestudio@gmail.com>
2025-04-20 21:13:54 +0900

    Update home.js

--- ac-info/home.js
+++ ac-info/home.js
@@ -46,3 +46,25 @@ function logout() {
 
     location.reload();
 }
+
+//club note
+const combined1 = grade + className + Name;
+
+const totalLength1 = combined1.length;
+
+const displayElement1 = document.querySelector(".storage-size1");
+if (displayElement1) {
+  displayElement1.textContent = `${totalLength1} B`;
+}
+
+//メモ帳
+const textFiles = localStorage.getItem("textFiles") || "";
+
+const combined2 = textFiles;
+
+const totalLength2 = combined2.length;
+
+const displayElement2 = document.querySelector(".storage-size2");
+if (displayElement2) {
+  displayElement2.textContent = `${totalLength2} B`;
+}
